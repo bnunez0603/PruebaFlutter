f1c4fc497a24599e3429ccb8c4a1d746bead9f72
Brian <bnunezr@alumnos.ceduc.cl.com>
2019-12-10 16:07:25 -0300

    primera carga

---
+++ lib/ventas.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class Ventas extends StatelessWidget {
+
+  @override
+  _Ventas createState() => new _Ventas();
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+
+}
+
+class _Ventas extends State<Ventas> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      
+    );
+  }
+
+}
+
